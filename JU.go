3ebd6214fa085e17218e570696b0d92e0cfb9a84
Yury <112165577+MangoTherapy@users.noreply.github.com>
2023-10-01 19:41:01 +0300

    Update JU.go

--- JU.go
+++ JU.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func POVERKA() {
+	var TP1 string
+	var TP2 string
+	TP1 = "123456"
+	TP2 = "654321"
+	fmt.Printf(TP1)
+	time.Sleep(5 * time.Second)
+	fmt.Printf(TP2)
+}
